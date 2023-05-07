f6151545cd83c9897739c0bddbd502a0dba3c3df
Anastasia Aleksandrova <anastasiayurievnaaleksandrova@gmail.com>
2023-05-08 00:33:09 +0300

    implemented dependence on a visit to the office

--- js/index.js
+++ js/index.js
@@ -14,13 +14,29 @@ const refs = {
   visitInOfficeCheckbox: document.getElementById("visitInOfficeCheckbox"),
   getCardInOfficeCheckbox: document.getElementById("getCardInOfficeCheckbox"),
   hasBlikCheckbox: document.getElementById("hasBlikCheckbox"),
-  deliveryMethod: document.querySelector("#getFinishCard"),
+  deliveryMethod: document.querySelector("#deliveryMethod"),
+  hasBlik: document.querySelector("#hasBlik"),
+  selectedDeliveryMethod: document.querySelector("option[selected]"),
+  textareaForOwnAdress: document.querySelector("#textareaForOwnAdress"),
+
+  deliveryMethodsList: {
+    ownAddresOption: document.querySelector("[data-addres='ownAddres']"),
+    getInOfficeOption: document.querySelector("[data-addres='getInOffice']"),
+    shippmentInPolandOption: document.querySelector(
+      "[data-addres='shippmentInPoland']"
+    ),
+    shipmentInternationalOption: document.querySelector(
+      "[data-addres='shipmentInternational']"
+    ),
+    shipmenttoTheRFOption: document.querySelector(
+      "[data-addres='shipmenttoTheRF']"
+    ),
+  },
 };
 
 const calculatorForm = document.querySelector(".calculatorForm");
 const totalPriceValue = document.querySelector("#totalPriceValue");
 totalPriceValue.value = `${BASE_COST + +e}zl`;
-
 calculatorForm.addEventListener("change", onChangeInputValue);
 calculatorForm.addEventListener("submit", onCalculatorFormSubmit);
 
@@ -30,13 +46,40 @@ function onCalculatorFormSubmit(event) {
   event.target.reset();
 }
 
-function onChangeInputValue(event) {
+function onChangeInputValue() {
   let procedure = null;
   if (
     refs.visitInOfficeCheckbox.checked &&
     !refs.expeditedClearanceCheckbox.checked
   ) {
     procedure = a;
+  }
+
+  if (refs.visitInOfficeCheckbox.checked) {
+    refs.hasBlik.classList.add("visually-hidden", "is-hidden");
+    refs.deliveryMethodsList.getInOfficeOption.setAttribute("selected", true);
+    refs.selectedDeliveryMethod?.setAttribute("selected", false);
+
+    for (const value in Object.values(refs.deliveryMethodsList)) {
+      Object.values(refs.deliveryMethodsList)[value].setAttribute(
+        "disabled",
+        true
+      );
+    }
+
+    addClass(refs.textareaForOwnAdress, "visually-hidden", "is-hidden");
+  }
+
+  if (!refs.visitInOfficeCheckbox.checked) {
+    for (const value in Object.values(refs.deliveryMethodsList)) {
+      Object.values(refs.deliveryMethodsList)[value].removeAttribute(
+        "disabled"
+      );
+    }
+    refs.hasBlik.classList.remove("visually-hidden", "is-hidden");
+    refs.deliveryMethodsList.getInOfficeOption.removeAttribute("selected");
+    refs.deliveryMethodsList.ownAddresOption.setAttribute("selected", true);
+    removeClass(refs.textareaForOwnAdress, "visually-hidden", "is-hidden");
   }
 
   if (
@@ -72,27 +115,42 @@ function onChangeInputValue(event) {
     procedure = e;
   }
 
+  if (refs.deliveryMethodsList.ownAddresOption.selected === false) {
+    addClass(refs.textareaForOwnAdress, "visually-hidden", "is-hidden");
+  }
+  if (refs.deliveryMethodsList.ownAddresOption.selected) {
+    removeClass(refs.textareaForOwnAdress, "visually-hidden", "is-hidden");
+  }
+
   const extraSum = procedure ? procedure : a;
   const deliverySum = refs.deliveryMethod.value;
 
-  console.log("минимальная стоимость:", BASE_COST);
-  console.log("доплата по выбранной процедуре", extraSum);
-  console.log("стоимость доставки", deliverySum);
+  // console.log("минимальная стоимость:", BASE_COST);
+  // console.log("доплата по выбранной процедуре", extraSum);
+  // console.log("стоимость доставки", deliverySum);
 
   totalPriceValue.value = `${BASE_COST + +extraSum + +deliverySum}zl`;
-  console.log("ИТОГОВАЯ СТОИМОСТЬ:", totalPriceValue.value);
+  // console.log("ИТОГОВАЯ СТОИМОСТЬ:", totalPriceValue.value);
+}
+
+function addClass(element, ...className) {
+  return element.classList.add(...className);
+}
+
+function removeClass(element, ...className) {
+  return element.classList.remove(...className);
 }
 
 // localStorage
-// что значит вариант доставки свой адресс?
+
 // нужна ли детализация итоговой суммы? (клиенту - нет)
 // шаблон ответа на почту (сумма и детализация)
 // что делает кнопка отправить? отправляет уведомление на почту?
 // если удаленная услуга, но есть блик, что делать? есть ли скидка 15?
 
-// если приезжает в офис, то без блика
-// если стоит галочка получить в офисе, то убираем способыт доставки (автоматом получение в офисе)
+// +++ если приезжает в офис, то без блика
+// если стоит галочка получить в офисе, то убираем способы доставки (автоматом получение в офисе)
 // уведомление, что уточним адрес после готовности карты
 // свой адресс - выводим поле для ввода адреса (индекс, город, улица, дом, квартира)
 
-//отправить ссылку на калк Сергею +++
+//+++ отправить ссылку на калк Сергею
