5f59bbcb745c1afad4aa112ae6bd3ae49c214efd
Anastasia Aleksandrova <anastasiayurievnaaleksandrova@gmail.com>
2023-05-13 22:51:56 +0300

    added php mailer

--- src/js/app.js
+++ src/js/app.js
@@ -65,14 +65,45 @@ calculatorForm.addEventListener("submit", onCalculatorFormSubmit);
 refs.prepaymentSum.value = `${BASE_COST} zl`;
 refs.surchargeSum.value = `0 zl`;
 
+function send(event, php) {
+  console.log("Отправка запроса");
+  event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+  var req = new XMLHttpRequest();
+  req.open("POST", php, true);
+  req.onload = function () {
+    if (req.status >= 200 && req.status < 400) {
+      json = JSON.parse(this.response); // Ебанный internet explorer 11
+      console.log(json);
+
+      // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+      if (json.result == "success") {
+        // Если сообщение отправлено
+        alert("Сообщение отправлено");
+      } else {
+        // Если произошла ошибка
+        alert("Ошибка. Сообщение не отправлено");
+      }
+      // Если не удалось связаться с php файлом
+    } else {
+      alert("Ошибка сервера. Номер: " + req.status);
+    }
+  };
+
+  // Если не удалось отправить запрос. Стоит блок на хостинге
+  req.onerror = function () {
+    alert("Ошибка отправки запроса");
+  };
+  req.send(new FormData(event.target));
+}
+
 function onCalculatorFormSubmit(event) {
   event.preventDefault();
   console.log("hi");
   event.currentTarget.reset();
   console.log("hello");
-  alert(
-    "Вашу заявку принято. Наш менеджер свяжется с вами для уточнения деталей."
-  );
+  // alert(
+  //   "Вашу заявку принято. Наш менеджер свяжется с вами для уточнения деталей."
+  // );
 }
 
 function onChangeInputValue() {
